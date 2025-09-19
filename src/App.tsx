7962c624c8f3324b473f9d2a203c57cc4f568847
leesangdal <asksa1256@gmail.com>
2025-09-19 16:13:40 +0900

    style: 토스트 폰트 크기 확대

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-6 py-10 bg-gray-50">
       <SearchContainer />
-      <Toaster position="bottom-center" richColors />
+      <Toaster
+        position="bottom-center"
+        richColors
+        toastOptions={{
+          className: "font-pretendard",
+        }}
+      />
       <CreateQuizModal />
     </main>
   );
