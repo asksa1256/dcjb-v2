e2bf988c4e71838a63b6e0f539a406362651cf4f
asksa1256@gmail.com <asksa1256@gmail.com>
2025-09-20 13:39:15 +0900

    style: 타이틀, copyright 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SearchContainer from "./components/SearchContainer";
 function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-6 py-10 bg-gray-50">
+      <h2 className="text-gray-400 text-lg text-left mb-8">대충족보</h2>
       <SearchContainer />
       <Toaster
         position="bottom-center"
@@ -13,6 +14,7 @@ function App() {
           className: "font-pretendard",
         }}
       />
+      <p className="my-4 text-center text-xs text-gray-300">&copy; 치즈꾸</p>
       <CreateQuizModal />
     </main>
   );
